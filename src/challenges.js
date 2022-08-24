b9ecf2c311f08bc23ae971558b5cca5ddcfd471b
brunaCFreitas <bruna_c_freitas@hotmail.com>
2022-08-24 14:57:51 -0300

    Adiciona resolução dos requisitos 1 e 2

--- src/challenges.js
+++ src/challenges.js
@@ -1,11 +1,17 @@
 // Desafio 1
-function compareTrue() {
+function compareTrue(value1, value2) {
   // seu código aqui
+  if (value1 && value2 === true) {
+    return true;
+  }
+  return false;
 }
 
 // Desafio 2
-function calcArea() {
+function calcArea(base, height) {
   // seu código aqui
+  let area = (base * height) / 2;
+  return area;
 }
 
 // Desafio 3
